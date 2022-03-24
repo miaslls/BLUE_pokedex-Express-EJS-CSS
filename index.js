3427531b2620d928c4e36ed118e2b0ed0d81460d
miaslls <97785569+miaslls@users.noreply.github.com>
2022-03-24 15:42:45 -0300

    Heroku Deploy

--- index.js
+++ index.js
@@ -3,7 +3,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
