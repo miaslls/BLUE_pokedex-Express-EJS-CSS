3dd75dc763d195ad4337e20b122fa74672f89218
miaslls <97785569+miaslls@users.noreply.github.com>
2022-03-24 01:40:00 -0300

    STYLIZED index.js

--- index.js
+++ index.js
@@ -1,39 +1,52 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
-app.set("view engine", "ejs");
-app.use(express.static(path.join(__dirname, "public")));
+app.set('view engine', 'ejs');
+app.use(express.static(path.join(__dirname, 'public')));
 
 const pokedex = [{
         id: 1,
-        name: "Bulbasaur",
-        type: "Grass",
-        img_url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
+        number: '001',
+        name: 'Bulbasaur',
+        category: 'Seed',
+        height: `2' 04"`,
+        weigth: '15.2lbs',
+        description: 'There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.',
+        type: 'Grass/Poison',
+        abilities: 'Overgrow',
+        img: 'img/pokemon_gifs/001.gif'
     },
     {
         id: 2,
-        name: "Charmander",
-        type: "Fire",
-        img_url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/004.png"
-    },
-    {
+        number: '002',
+        name: 'IVYSAUR',
+        category: 'Seed',
+        height: `3' 03"`,
+        weigth: '28.7lbs',
+        description: 'When the bulb on its back grows large, it appears to lose the ability to stand on its hind legs.',
+        type: 'Grass/Poison',
+        abilities: 'Overgrow',
+        img: 'img/pokemon_gifs/002.gif'
+    }, {
         id: 3,
-        name: "Squirtle",
-        type: "Water",
-        img_url: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/007.png"
+        number: '003',
+        name: 'Venusaur',
+        category: 'Seed',
+        height: `6' 07"`,
+        weigth: '220.5lbs',
+        description: 'Its plant blooms when it is absorbing solar energy. It stays on the move to seek sunlight.',
+        type: 'Grass/Poison',
+        abilities: 'Overgrow',
+        img: 'img/pokemon_gifs/003.gif'
     }
-]
+];
 
 // 🚨📌📌📌 ----- ROTAS -----
 
-app.get("/", (req, res) => {
-    res.render("index", { pokedex });
-});
-
-app.post("/add", (req, res) => {
-
+app.get('/', (req, res) => {
+    res.render('index', { pokedex });
 });
 
 app.listen(port, () => console.log(`Server running at http://localhost:${port}`));
